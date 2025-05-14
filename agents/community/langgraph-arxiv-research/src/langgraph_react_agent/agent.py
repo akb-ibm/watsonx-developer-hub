61409b721f299bdab6b1719ccd0f8762299cf0d9
Kajetan Ozog <Kajetan.Ozog@ibm.com>
2025-05-14 12:51:19 +0200

    remove MemorySaver from community/langgraph_arxiv_research
    Signed-off-by: Kajetan Ozog <Kajetan.Ozog@ibm.com>

--- agents/community/langgraph-arxiv-research/src/langgraph_react_agent/agent.py
+++ agents/community/langgraph-arxiv-research/src/langgraph_react_agent/agent.py
@@ -17,15 +17,13 @@ def get_graph_closure(client: APIClient, model_id: str) -> Callable:
     # Define system prompt
     default_system_prompt = "You are a helpful AI Research assistant, please respond to the user's query to the best of your ability! Execute a tool call whenever you see fit. When using tools, make sure to format the URL to an arXiv research paper like 'https://arxiv.org/html/2501.12948v1'"
 
-    # Initialise memory saver
-    memory = MemorySaver()
 
     def get_graph(system_prompt=default_system_prompt) -> CompiledGraph:
         """Get compiled graph with overwritten system prompt, if provided"""
 
         # Create instance of compiled graph
         return create_react_agent(
-            chat, tools=TOOLS, checkpointer=memory, state_modifier=system_prompt
+            chat, tools=TOOLS, state_modifier=system_prompt
         )
 
     return get_graph

72599d1bef0e22de713229ef464712e82d5be1e3
Rafalll-Maciasz <Rafalll.Maciasz@ibm.com>
2025-05-21 11:25:55 +0200

    fix: run pytest from poetry level

--- scripts/setup-agent-test.py
+++ scripts/setup-agent-test.py
@@ -1,6 +1,5 @@
 import os
 import subprocess
-import sys
 from utils import install_package
 
 # Install poetry required for deployment
@@ -9,5 +8,5 @@ install_package("poetry")
 # Install the template with poetry
 agent_workdir = os.environ["AGENT_WORKDIR"]
 subprocess.check_call(
-    [sys.executable, "-m", "poetry", "install", "--with", "dev", "--directory", agent_workdir]
+    ["poetry", "install", "--with", "dev", "--directory", agent_workdir]
 )

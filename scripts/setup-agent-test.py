9ed74967195fbcc3360e01600288ceb910f98315
Rafalll-Maciasz <Rafalll.Maciasz@ibm.com>
2025-05-21 10:23:21 +0200

    fix: run pytest via python to avoid command not found error in workflow

--- scripts/setup-agent-test.py
+++ scripts/setup-agent-test.py
@@ -6,7 +6,7 @@ from utils import install_package
 # Install poetry required for deployment
 install_package("poetry")
 
-# Install the template
+# Install the template with poetry
 agent_workdir = os.environ["AGENT_WORKDIR"]
 subprocess.check_call(
     [sys.executable, "-m", "poetry", "install", "--with", "dev", "--directory", agent_workdir]

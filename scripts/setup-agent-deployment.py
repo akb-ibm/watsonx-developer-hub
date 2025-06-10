cf8e3955717e8e65bea5afdff6fc16953746eafc
Rafalll-Maciasz <Rafalll.Maciasz@ibm.com>
2025-06-10 20:51:37 +0200

    fix: remove install_package method from utils.py

--- scripts/setup-agent-deployment.py
+++ scripts/setup-agent-deployment.py
@@ -2,9 +2,6 @@ import os
 from utils import create_config, install_package
 
 
-# Install CLI
-install_package("ibm-watsonx-ai-cli")
-
 # Setup config.toml
 agent_workdir = os.getenv("AGENT_WORKDIR", ".")
 config_file = f"{agent_workdir}/config.toml"
--- scripts/setup-template-test.py
+++ scripts/setup-template-test.py
@@ -3,9 +3,6 @@ import subprocess
 from utils import install_package
 
 
-# Install CLI
-install_package("ibm-watsonx-ai-cli")
-
 # Install the template with poetry
 agent_workdir = os.getenv("AGENT_WORKDIR", ".")
 os.chdir(agent_workdir)

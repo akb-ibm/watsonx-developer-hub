fbb1a15da9de8cac0b4d43a5d6a8b92252e6807d
Rafalll-Maciasz <Rafalll.Maciasz@ibm.com>
2025-06-11 12:39:25 +0200

    fix: provide WATSONX_URL also to online parameters section in config file

--- agents/base/autogen-agent/scripts/setup-config.py
+++ agents/base/autogen-agent/scripts/setup-config.py
@@ -19,13 +19,21 @@ def create_config(template_file: str, target_file: str):
     with open(target_file, "r") as f:
         config = parse(f.read())
 
-    config["deployment"]["watsonx_apikey"] = os.getenv("WATSONX_API_KEY", "")
-    config["deployment"]["watsonx_url"] = os.getenv("WATSONX_URL", "")
-    config["deployment"]["space_id"] = os.getenv("WATSONX_SPACE_ID", "")
-    config["deployment"]["deployment_id"] = os.getenv("DEPLOYMENT_ID", "")
+    deployment_section_name = "deployment"
+    
+    if deployment_section_name not in config.keys():
+        raise KeyError(f"Configuration section {deployment_section_name} not found in {target_file}")
+    
+    config[deployment_section_name]["watsonx_apikey"] = os.getenv("WATSONX_API_KEY", "")
+    config[deployment_section_name]["watsonx_url"] = os.getenv("WATSONX_URL", "")
+    config[deployment_section_name]["space_id"] = os.getenv("WATSONX_SPACE_ID", "")
+    config[deployment_section_name]["deployment_id"] = os.getenv("DEPLOYMENT_ID", "")
+
+    # Map the same url for online parameters
+    config[deployment_section_name]["online"]["parameters"]["url"] = config[deployment_section_name]["watsonx_url"]
 
     # TODO - consider changing sw_spec name with timestamp, as a temp solution set overwrite flag
-    config["deployment"]["software_specification"]["overwrite"] = True
+    config[deployment_section_name]["software_specification"]["overwrite"] = True
 
     with open(target_file, "w") as f:
         f.write(dumps(config))

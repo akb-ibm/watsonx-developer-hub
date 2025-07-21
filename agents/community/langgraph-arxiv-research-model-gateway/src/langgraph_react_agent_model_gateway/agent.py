3289fad242373e4f2e1c2b23645dc90bf41a84f8
Rafal Chrzanowski <Rafal.Chrzanowski@ibm.com>
2025-07-21 09:16:25 +0200

    fix: Downgraded langgraph version
    Signed-off-by: Rafal Chrzanowski <Rafal.Chrzanowski@ibm.com>

--- agents/community/langgraph-arxiv-research-model-gateway/src/langgraph_react_agent_model_gateway/agent.py
+++ agents/community/langgraph-arxiv-research-model-gateway/src/langgraph_react_agent_model_gateway/agent.py
@@ -2,7 +2,7 @@ from typing import Callable
 
 from ibm_watsonx_ai import APIClient
 from langchain_ibm import ChatWatsonx
-from langgraph.graph.state import CompiledStateGraph
+from langgraph.graph.graph import CompiledGraph
 from langgraph.prebuilt import create_react_agent
 
 from langgraph_react_agent_model_gateway import TOOLS
@@ -22,10 +22,10 @@ def get_graph_closure(client: APIClient, model: str) -> Callable:
         "like 'https://arxiv.org/html/2501.12948v1'"
     )
 
-    def get_graph(system_prompt=default_system_prompt) -> CompiledStateGraph:
+    def get_graph(system_prompt=default_system_prompt) -> CompiledGraph:
         """Get compiled graph with overwritten system prompt, if provided"""
 
         # Create instance of compiled graph
-        return create_react_agent(chat, tools=TOOLS, prompt=system_prompt)
+        return create_react_agent(chat, tools=TOOLS, state_modifier=system_prompt)
 
     return get_graph
